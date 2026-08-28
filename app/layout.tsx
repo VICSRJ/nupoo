d1b8a5bbabd70f34f578fd5f9933bacab97ca768
vics <ruzickajakub95@gmail.com>
2026-08-28 02:01:43 +0200

    feat: switch editor icons to Font Awesome CDN

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,18 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import Script from 'next/script'
 
 export const metadata: Metadata = { title: 'Nupoo', description: 'Notion-like block editor' }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="cs">
-      <body>
-        <Script
-          src="https://code.iconify.design/iconify-icon/2.1.0/iconify-icon.min.js"
-          strategy="afterInteractive"
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
         />
-        {children}
-      </body>
+      </head>
+      <body>{children}</body>
     </html>
   )
 }
